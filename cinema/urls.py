a2ecf6901493413a87f9814461551b143e87ad47
Frezworx <frezworx@gmail.com>
2024-12-19 19:09:53 +0200

    Add viewsets and routers for movies and cinema halls
    Replaced function-based views with viewsets for better modularity and functionality. Updated URLs to use DRF routers and streamlined endpoint management. Introduced new API views for genres and actors using class-based views.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import GenreAPIView, ActorGenericAPIView, CinemaHallViewSet, \
+    MovieViewSet
+
+router = DefaultRouter()
+router.register("cinema-halls", CinemaHallViewSet, basename="cinema-hall")
+router.register("movies", MovieViewSet, basename="movies")
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreAPIView.as_view(), name="genre-list"),
+    path("actors/", ActorGenericAPIView.as_view(), name="actor-list"),
+    path("", include(router.urls)),
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,45 +1,61 @@
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+)
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+
+from cinema.models import Genre, Movie, CinemaHall, Actor
+from cinema.serializers import (
+    GenreSerializer,
+    MovieSerializer,
+    CinemaHallSerializer,
+    ActorSerializer,
+)
 
 
 class GenreAPIView(APIView):
     def get(self, request):
-        pass
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
-        pass
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# @api_view(["GET", "POST"])
-# def movie_list(request):
-#     if request.method == "GET":
-#         movies = Movie.objects.all()
-#         serializer = MovieSerializer(movies, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     if request.method == "POST":
-#         serializer = MovieSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# @api_view(["GET", "PUT", "DELETE"])
-# def movie_detail(request, pk):
-#     movie = get_object_or_404(Movie, pk=pk)
-#
-#     if request.method == "GET":
-#         serializer = MovieSerializer(movie)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#     if request.method == "PUT":
-#         serializer = MovieSerializer(movie, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     if request.method == "DELETE":
-#         movie.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ActorGenericAPIView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class CinemaHallViewSet(
+    GenericViewSet,
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
