1ad824b4aae0ed10b0144dbb4ff4c73f5b87c05e
Frezworx <frezworx@gmail.com>
2024-12-19 17:29:16 +0200

    Add new models: Actor, Genre, CinemaHall, and MovieHall
    Introduced models to represent actors, genres, cinema halls, and movies with their attributes and relationships. These changes lay the foundation for managing cinemas, movie metadata, and associations within the application.

--- cinema/models.py
+++ cinema/models.py
@@ -8,3 +8,35 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=63)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class MovieHall(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movie_halls")
+    genres = models.ManyToManyField(Genre, related_name="movie_halls")
+    duration = models.IntegerField()
